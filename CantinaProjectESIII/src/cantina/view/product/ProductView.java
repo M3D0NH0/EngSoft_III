4628adef66507864082b815414ffd9c1319f2413
Henrique Schwab - ADS - <hschwabg@yahoo.com.br>
2014-09-11 18:25:52 +0100

    upup

--- CantinaProjectESIII/src/cantina/view/product/ProductView.java
+++ CantinaProjectESIII/src/cantina/view/product/ProductView.java
@@ -1,6 +1,9 @@
 package cantina.view.product;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 /**
  * 
@@ -8,14 +11,29 @@ import javax.swing.JFrame;
  *
  */
 public class ProductView extends JFrame {
+	public ProductView() {
+	}
 
 	private ProductTable produtosRela;
+	private JMenuBar menuBar;
+	private JMenu mnMenu;
+	private JMenuItem mntmProdutos;
+	private JMenuItem mntmSair;
+	
 
 	public void iniciaComponentes() {
 		setResizable(false);
 		setTitle("Cantina Project");
 		setSize(1020, 665);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		mnMenu = new JMenu("Menu");
+		menuBar.add(mnMenu);
+		mntmProdutos = new JMenuItem("Produtos");
+		mntmSair = new JMenuItem("Sair");
+		mnMenu.add(mntmProdutos);
+		mnMenu.add(mntmSair);
 		produtosRela = new ProductTable();
 		getContentPane().add(produtosRela);
 		setLocationRelativeTo(null);
