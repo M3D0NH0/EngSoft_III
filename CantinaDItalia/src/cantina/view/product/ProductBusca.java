53c93525eceaa8d271d9f708c704710d70da3630
Henrique S. <hschwabg@yahoo.com.br>
2014-09-15 20:43:28 -0300

    User Story 01 up

--- CantinaDItalia/src/cantina/view/product/ProductBusca.java
+++ CantinaDItalia/src/cantina/view/product/ProductBusca.java
@@ -2,9 +2,11 @@ package cantina.view.product;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.Closeable;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -57,10 +59,17 @@ public class ProductBusca extends JPanel {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				if (arg0.getSource().equals(btnBuscar)) {
+					if (textFieldBusca.getText() == null
+							|| textFieldBusca.getText().equals("")) {
+						JOptionPane.showMessageDialog(null, "Data inválida");
+					} else {
 					data = textFieldBusca.getText();
 					ProductTable produtosRela = new ProductTable(data);
 					ProductView.splitPane.setRightComponent(produtosRela);
 					ProductView.splitPane.setDividerLocation(200);
+					textFieldBusca.setText(null);
+					JOptionPane.showMessageDialog(null, "Busca realizada");
+					}
 				}
 			}
 		});
