57ff1252bbfb1d4c944c9390994bfcf9a86fbda1
msndogui@hotmail.com.br <msndogui@hotmail.com.br>
2014-10-08 22:50:35 -0300

    modificacao na classe de cadastro

--- CantinaDItalia/src/cantina/view/ProductInsert.java
+++ CantinaDItalia/src/cantina/view/ProductInsert.java
@@ -1,9 +1,15 @@
 package cantina.view;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
+import cantina.model.Product;
 
 /**
  * 
@@ -42,18 +48,77 @@ public class ProductInsert extends JPanel{
 	
 	public void addComponetes(){
 		setLayout(null);
+		
+		lb_nome.setBounds(26, 77, 208, 23);
 		add(lb_nome);
+		lb_nome.setForeground(Color.WHITE);
+		
+		tx_nome.setBounds(30, 77, 208, 23);
 		add(tx_nome);
+		tx_nome.setForeground(Color.WHITE);
+		
+		lb_tipo.setBounds(30, 77, 208, 23);
 		add(lb_tipo);
+		lb_tipo.setForeground(Color.WHITE);
+		
+		tx_tipo.setBounds(30, 77, 208, 23);
 		add(tx_tipo);
+		tx_tipo.setForeground(Color.WHITE);
+		
+		lb_validade.setBounds(30, 77, 208, 23);
 		add(lb_validade);
+		lb_validade.setForeground(Color.WHITE);
+		
+		tx_validade.setBounds(30, 77, 208, 23);
 		add(tx_validade);
+		tx_validade.setForeground(Color.WHITE);
+		
+		lb_quantidade.setBounds(30, 77, 208, 23);
 		add(lb_quantidade);
+		lb_quantidade.setForeground(Color.WHITE);
+		
+		tx_quantidade.setBounds(30, 77, 208, 23);
 		add(tx_quantidade);
+		tx_quantidade.setForeground(Color.WHITE);
+		
+		lb_preco.setBounds(30, 77, 208, 23);
 		add(lb_preco);
+		lb_preco.setForeground(Color.WHITE);
+		
+		tx_preco.setBounds(30, 77, 208, 23);
 		add(tx_preco);
+		tx_preco.setForeground(Color.WHITE);
+		
+		bt_adicionar.setBounds(30, 77, 208, 23);
 		add(bt_adicionar);
+		bt_adicionar.setForeground(Color.WHITE);
+		bt_adicionar.addActionListener(new tratadorBotoes());
+		
+		bt_limpar.setBounds(30, 77, 208, 23);
 		add(bt_limpar);
+		bt_limpar.setForeground(Color.WHITE);
+		bt_limpar.addActionListener(new tratadorBotoes());
+		
+	}
+	
+	public class tratadorBotoes implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource().equals(bt_adicionar)){
+				Product produto = new Product();
+				produto.
+				
+			}else if(e.getSource().equals(bt_limpar)){
+				tx_nome.setText(null);
+				tx_tipo.setText(null);
+				tx_quantidade.setText(null);
+				tx_validade.setText(null);
+				tx_preco.setText(null);
+			}else{
+				
+			}
+		}
 	}
 	
 	
